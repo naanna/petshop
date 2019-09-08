138ee0ec68a13885bab898fc166059a1f49e4075
mxy <546679106@qq.com>
2019-09-08 19:47:01 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ function resolve(dir) {
     return path.join(__dirname, dir);
 }
 module.exports = {
+    publicPath: '/',
     lintOnSave: true,
     chainWebpack: (config) => {
         config.resolve.alias
