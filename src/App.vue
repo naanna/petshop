52c9b5658df39d18d69e7b3b5fb069411e124937
mxy <546679106@qq.com>
2019-09-26 22:55:27 +0800

    update login loading

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,26 @@ export default {
   data() {
     return {};
   },
-  methods: {}
+  watch: {
+    $route(to, from) {
+      if (to.name == "main" && from.name == "Login") {
+        this.openFullScreen();
+      }
+    }
+  },
+  methods: {
+    openFullScreen() {
+      const loading = this.$loading({
+        lock: true,
+        text: "正在努力加载页面",
+        spinner: "el-icon-loading",
+        background: "rgba(0, 0, 0, 1)"
+      });
+      setTimeout(() => {
+        loading.close();
+      }, 1500);
+    }
+  }
 };
 </script>
 
