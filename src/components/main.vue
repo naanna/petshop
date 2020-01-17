812537bed4d04b4759252126f810a223b6dcba1e
naanna <546679106@qq.com>
2020-01-17 17:42:13 +0800

    首页添加日历和天气预报，引入高德地图第三方接口

--- src/components/main.vue
+++ src/components/main.vue
@@ -73,25 +73,8 @@
           <span class="titleclass">宝贝照片</span>
         </div>
         <el-row :gutter="20" class="row">
-          <el-col :span="8">
-            <img src="https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/cat1.png" class="picture">
-          </el-col>
-          <el-col :span="8">
-            <img src="https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/dog1.png" class="picture">
-          </el-col>
-          <el-col :span="8">
-            <img src="https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/pig1.png" class="picture">
-          </el-col>
-        </el-row>
-        <el-row :gutter="20" class="row">
-          <el-col :span="8">
-            <img src="https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/cat2.png" class="picture">
-          </el-col>
-          <el-col :span="8">
-            <img src="https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/dog2.png" class="picture">
-          </el-col>
-          <el-col :span="8">
-            <img src="https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/pig2.png" class="picture">
+          <el-col :span="8" v-for="(item,index) in picturelist" :key="index">
+            <el-image class="picture" :src="item" :preview-src-list="[item]"></el-image>
           </el-col>
         </el-row>
       </el-col>
@@ -111,7 +94,7 @@
             <span class="titleclass">用户活动</span>
           </div>
           <div>
-            <div class="user userhead" v-for="(item,index) in userdata" :key="{index}">
+            <div class="user userhead" v-for="(item,index) in userdata" :key="index">
               <div>
                 <span>会员</span>
                 <span class="usernamecolor join">{{item.nickname}}</span>
@@ -156,6 +139,59 @@
         </div>
       </el-col>
     </el-row>
+    <el-row :gutter="20" class="row">
+      <el-col :lg="{span:12}" :md="{span:24}">
+        <div class="userdiv">
+          <div class="userhead">
+            <i class="el-icon-date icon"></i>
+            <span class="titleclass">日程活动</span>
+          </div>
+          <el-calendar>
+            <template slot="dateCell" slot-scope="{date, data}">
+              <p>
+                {{ data.day.split('-').slice(2)[0] }}
+                <br>
+                {{dealMyDate(data.day)}}
+              </p>
+            </template>
+          </el-calendar>
+        </div>
+      </el-col>
+      <el-col :lg="{span:12}" :md="{span:24}">
+        <div class="userdiv">
+          <div class="userhead">
+            <i class="el-icon-date icon"></i>
+            <span class="titleclass">天气预报--福州闽侯县</span>
+          </div>
+          <el-row class="weather" v-for="(item,index) in weatherdata.casts" :key="index">
+            <el-col :span="6">{{item.date.split('-').slice(2)[0]}}日({{gettimestr(item.week)}})</el-col>
+            <el-col :span="6">
+              <div style="display:flex;justify-content:center;">
+                <span v-if="item.dayweather==item.nightweather">
+                  <i :class="getweathericon(item.dayweather)" style="font-size:30px;"></i>
+                  {{item.dayweather}}
+                </span>
+                <span v-else>
+                  {{item.dayweather}}
+                  <i
+                    :class="getweathericon(item.dayweather)"
+                    style="font-size:30px;"
+                  ></i>
+                  转
+                  {{item.nightweather}}
+                  <i
+                    :class="getweathericon(item.nightweather)"
+                    style="font-size:30px;"
+                  ></i>
+                </span>
+              </div>
+            </el-col>
+            <el-col :span="6">{{item.nighttemp}}℃～{{item.daytemp}}℃</el-col>
+            <el-col :span="6">{{item.daywind}}</el-col>
+          </el-row>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -183,14 +219,119 @@ export default {
         ]
       },
       data: [],
-      userdata: [{ nickname: "123", regday: "2007-08-05" },{ nickname: "123", regday: "2007-08-05" }, ]
+      userdata: [
+        { nickname: "123", regday: "2007-08-05" },
+        { nickname: "123", regday: "2007-08-05" }
+      ],
+      picturelist: [
+        "https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/cat1.png",
+        "https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/dog1.png",
+        "https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/pig1.png",
+        "https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/cat2.png",
+        "https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/dog2.png",
+        "https://mmzdpicture.oss-cn-hangzhou.aliyuncs.com/pig2.png"
+      ],
+      calendarDate: [
+        { date: "2020-01-17", content: "123" },
+        { date: "2020-01-12", content: "123" },
+        { date: "2020-01-15", content: "123" }
+      ],
+      weatherdata: {}
     };
   },
   created() {
     this.permissions = this.$store.state.permissions;
-    this.getnums();
+    //this.getnums();
+    this.getweather();
   },
   methods: {
+    getweather() {
+      this.axios("gaode/v3/weather/weatherInfo?parameters", {
+        params: {
+          key: "8bbfdf6309caef7066348deed2e1f503",
+          city: "350121",
+          extensions: "all"
+        }
+      }).then(res => {
+        if (res.data.status == 1) {
+          this.weatherdata = res.data.forecasts[0];
+        }
+      });
+    },
+    gettimestr(i) {
+      let day = "";
+      switch (parseInt(i)) {
+        case 1:
+          day = "星期一";
+          break;
+        case 2:
+          day = "星期二";
+          break;
+        case 3:
+          day = "星期三";
+          break;
+        case 4:
+          day = "星期四";
+          break;
+        case 5:
+          day = "星期五";
+          break;
+        case 6:
+          day = "星期六";
+          break;
+        case 7:
+          day = "星期日";
+          break;
+      }
+      return day;
+    },
+    getweathericon(i) {
+      let weatherstr = "";
+      switch (i) {
+        case "阴":
+          weatherstr = "el-icon-partly-cloudy";
+          break;
+        case "晴":
+          weatherstr = "el-icon-sunny";
+          break;
+        case "晴间多云":
+          weatherstr = "el-icon-cloudy-and-sunny";
+          break;
+        case "多云":
+          weatherstr = "el-icon-cloudy";
+          break;
+        case "阵雨":
+          weatherstr = "el-icon-light-rain";
+          break;
+        case "雷阵雨":
+          weatherstr = "el-icon-light-rain";
+          break;
+        case "小雨":
+          weatherstr = "el-icon-light-rain";
+          break;
+        case "中雨":
+          weatherstr = "el-icon-light-rain";
+          break;
+        case "大雨":
+          weatherstr = "el-icon-heavy-rain";
+          break;
+        case "暴雨":
+          weatherstr = "el-icon-heavy-rain";
+          break;
+      }
+      return weatherstr;
+    },
+    dealMyDate(v) {
+      let len = this.calendarDate.length;
+      let res = "";
+      for (let i = 0; i < len; i++) {
+        if (this.calendarDate[i].date == v) {
+          res = this.calendarDate[i].content;
+          break;
+        }
+      }
+      return res;
+    },
     getnums() {
       this.axios
         .get("/api/getindex")
@@ -275,6 +416,7 @@ export default {
 .picture {
   width: 160px;
   height: 160px;
+  margin-bottom: 20px;
 }
 .chart {
   width: 100%;
@@ -292,7 +434,7 @@ export default {
   border-bottom: 1px solid #dddddd;
 }
 .userdiv {
-  background: #e4e0e0;
+  background: #ebeff3;
   border-radius: 10px;
   border: 1px solid #dddddd;
 }
@@ -305,5 +447,12 @@ export default {
 .row {
   margin-top: 20px;
   margin-bottom: 20px;
+}
+.weather {
+  background: #ffffff;
+  text-align: center;
+  font-size: 18px;
+  color: #767676;
+  padding: 20px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,44 @@
 const path = require('path');
 
 function resolve(dir) {
-    return path.join(__dirname, dir);
+  return path.join(__dirname, dir);
 }
 module.exports = {
-    publicPath: '/',
-    lintOnSave: true,
-    productionSourceMap: false,
-    chainWebpack: (config) => {
-        config.resolve.alias
-            .set('@components', resolve('src/components'))
-            .set('@assets', resolve('src/assets'))
-            .set('@picture', resolve('src/assets/picture'))
-            .set('@common', resolve('src/common'))
-            .set('@store', resolve('src/store'))
-            .set('@router', resolve('src/router'))
-            .set('@', resolve('src'))
-    },
-    devServer: {
-        // 设置主机地址
-        //   host: '127.1.0.1',
-        // 设置默认端口
-        //port: 3000,
-        // 设置代理
-        proxy: {
-            '/api': {
-                // 目标 API 地址
-                target: 'http://127.0.0.1:3000',
-                // 如果要代理 websockets
-                ws: true,
-                // 将主机标头的原点更改为目标URL
-                changeOrigin: false
-            }
+  publicPath: '/',
+  lintOnSave: true,
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@components', resolve('src/components'))
+      .set('@assets', resolve('src/assets'))
+      .set('@picture', resolve('src/assets/picture'))
+      .set('@common', resolve('src/common'))
+      .set('@store', resolve('src/store'))
+      .set('@router', resolve('src/router'))
+      .set('@', resolve('src'));
+  },
+  devServer: {
+    // 设置主机地址
+    //   host: '127.1.0.1',
+    // 设置默认端口
+    //port: 3000,
+    // 设置代理
+    proxy: {
+      '/gaode': {
+        target: 'https://restapi.amap.com', //访问地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/gaode': ''
         }
-    },
+      },
+      '/api': {
+        // 目标 API 地址
+        target: 'http://127.0.0.1:3000',
+        // 如果要代理 websockets
+        ws: true,
+        // 将主机标头的原点更改为目标URL
+        changeOrigin: false
+      }
+    }
+  }
 };
