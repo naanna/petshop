5585a6fb338c9e2ff6474911406dbefbb27457e2
mxy <546679106@qq.com>
2020-03-03 17:14:43 +0800

    聊天室功能优化

--- src/components/chat/index.vue
+++ src/components/chat/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="chat-box">
-    <p class="title">聊天室人数：{{count}}</p>
+    <p class="title">公共聊天室人数：{{count}}</p>
     <div class="msg-box" ref="msg-box">
       <el-scrollbar class="scroll" ref="myScrollbar">
         <div v-for="(i,index) in list" :key="index">
@@ -11,7 +11,7 @@
           <div class="user-msg-box" :style="i.userId == userId?'justify-content: flex-end;':''">
             <!-- 左边的 -->
             <div class="user-msg-content" v-if="i.userId != userId" style="padding-right:10px;">
-              <el-avatar size="small"></el-avatar>
+              <el-avatar size="small" :src="i.userPicture"></el-avatar>
               <span
                 class="triangle"
                 style="border-color: transparent #f1f1f1 transparent transparent;right:0;"
@@ -19,7 +19,7 @@
             </div>
             <!-- 文字 -->
             <div style="flex:25" :style="i.userId == userId?'text-align:right;':''">
-              <span class="user-msg-username">木木周</span>
+              <span class="user-msg-username">{{i.nickName}}</span>
               <span class="user-msg" :class="i.userId == userId?'right':'left'">{{i.content}}</span>
             </div>
             <!-- 右边的 -->
@@ -28,13 +28,14 @@
                 class="triangle"
                 style="border-color: transparent transparent transparent #d1f2fd;left:0;"
               ></span>
-              <el-avatar size="small"></el-avatar>
+              <el-avatar size="small" :src="i.userPicture">></el-avatar>
             </div>
           </div>
         </div>
       </el-scrollbar>
     </div>
-    <div class="input-box">
+    <p class="warn">您的发言将所有人可见！拒绝发布有害/违法/黄色信息！</p>
+    <div class="center">
       <el-input v-model="contentText" placeholder="请输入内容" ref="sendMsg" class="input"></el-input>
       <el-button @click="sendText()" style="margin-left:30px;">发送</el-button>
     </div>
@@ -45,23 +46,21 @@
 export default {
   data() {
     return {
-      timer: "",
       ws: null,
       count: 0,
       userId: null, //当前用户ID
+      userPicture: null, //当前用户照片
+      nickName: null, //当前用户昵称
       list: [], //聊天记录的数组
       contentText: "" //input输入的值
     };
   },
   created() {
-    this.getUserID();
+    this.getUser();
     this.initWebSocket();
   },
-  mounted() {
-    this.timer = setInterval(this.initWebSocket, 60000);
-  },
-  beforeDestroy() {
-    clearInterval(this.timer);
+  destroyed() {
+    this.ws.close(); //离开路由之后断开websocket连接
   },
   methods: {
     //合并一分钟内对话
@@ -73,30 +72,17 @@ export default {
       }
       return true;
     },
-    //根据时间戳作为当前用户ID----连上了此时应该用admin来当用户ID
-    //需要修改！！
-    getUserID() {
-      let time = new Date().getTime();
-      this.userId = time;
-    },
-    //根据userID生成一个随机头像
-    getUserHead(id, type) {
-      let ID = String(id);
-      if (type == "bck") {
-        return Number(ID.substring(ID.length - 3));
-      }
-      if (type == "polygon") {
-        return Number(ID.substring(ID.length - 2));
-      }
-      if (type == "rotate") {
-        return Number(ID.substring(ID.length - 3));
-      }
+    //建立用户信息
+    getUser() {
+      this.userId = this.$store.state.username;
+      this.userPicture = this.$store.state.picture;
+      this.nickName = this.$store.state.nickname;
     },
     //滚动条到底部
     scrollBottm() {
-      this.$refs["myScrollbar"].wrap.scrollTop = this.$refs[
+      this.$refs["myScrollbar"].$refs["wrap"].scrollTop = this.$refs[
         "myScrollbar"
-      ].wrap.scrollHeight;
+      ].$refs["wrap"].scrollHeight;
     },
     //发送聊天信息
     sendText() {
@@ -107,6 +93,8 @@ export default {
       }
       let params = {
         userId: _this.userId,
+        userPicture: _this.userPicture,
+        nickName: _this.nickName,
         msg: _this.contentText,
         createtime: new Date().getTime()
       };
@@ -125,13 +113,13 @@ export default {
         let ws = new WebSocket("ws://192.168.80.1:8181");
         _this.ws = ws;
         ws.onopen = function(e) {
-          console.log("服务器连接成功");
+          console.log("聊天室服务器连接成功");
         };
         ws.onclose = function(e) {
-          console.log("服务器连接关闭");
+          console.log("聊天室服务器连接关闭");
         };
         ws.onerror = function() {
-          console.log("服务器连接出错");
+          console.log("聊天室服务器连接出错");
         };
         ws.onmessage = function(e) {
           //接收服务器返回的数据
@@ -139,12 +127,13 @@ export default {
           if (resData.funName == "userCount") {
             _this.count = resData.users;
             _this.list = resData.chat;
-            console.log(resData.chat);
           } else {
             _this.list = [
               ..._this.list,
               {
                 userId: resData.userId,
+                userPicture: resData.userPicture,
+                nickName: resData.nickName,
                 content: resData.msg,
                 createtime: resData.createtime
               }
@@ -157,6 +146,7 @@ export default {
         };
       }
     },
+    //转化时间
     timestampFormat(timestamp) {
       function zeroize(num) {
         return (String(num).length == 1 ? "0" : "") + num;
@@ -218,22 +208,22 @@ export default {
 </script>
 
 <style scoped>
+.chat-box {
+  min-width: 800px;
+}
 .title {
   text-align: center;
+  margin-top: 0;
 }
 .msg-box {
   width: 800px;
   height: 400px;
   border: 1px solid #e5e5e5;
-  margin: 0 auto;
+  margin: 10px auto;
   padding: 10px 0;
 }
 .input {
   width: 400px;
-}
-.input-box {
-  margin: 20px;
-  text-align: center;
 }
 .msg-list {
   margin: 10px;
@@ -284,5 +274,10 @@ export default {
   display: inline-block;
   position: absolute;
   bottom: -15px;
+}
+.warn {
+  font-size: 10px;
+  color: #636e72;
+  margin: 0 0 10px 1px;
 }
 </style>
